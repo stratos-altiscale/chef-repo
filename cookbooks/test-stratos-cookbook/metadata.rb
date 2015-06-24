4815e4c4f0859023d5b98b38f5231c1c8793e68f
Stratos Dimopoulos <stratos@altiscale.com>
2015-06-24 13:26:13 -0700

    playing with chef cookbooks

---
+++ cookbooks/test-stratos-cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'test-stratos-cookbook'
+maintainer       'stratos'
+maintainer_email 'stratos@altiscale.com'
+license          ''
+description      'Installs/Configures test-stratos-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/test-stratos-cookbook/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: test-stratos-cookbook
+# Recipe:: default
+#
+# Copyright 2015, stratos
+#
